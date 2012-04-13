901f7a9d4238489e85617dd553e2af89f3bf0c8c
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-04-13 18:48:10 +0200

    Fix typo.

--- SundownNet/HtmlRenderMode.cs
+++ SundownNet/HtmlRenderMode.cs
@@ -8,7 +8,7 @@ namespace Sundown
 		HTML_SKIP_STYLE       = (1 << 1),
 		HTML_SKIP_IMAGES      = (1 << 2),
 		HTML_SKIP_LINKS       = (1 << 3),
-		HTML_SKIP_EXPAND_TABs = (1 << 4),
+		HTML_SKIP_EXPAND_TABS = (1 << 4),
 		HTML_SAFELINK         = (1 << 5),
 		HTML_TOC              = (1 << 6),
 		HTML_HARD_WRAP        = (1 << 7),
@@ -37,13 +37,13 @@ namespace Sundown
 			if (SkipStyle)      ret |= (uint)html_render_mode.HTML_SKIP_STYLE;
 			if (SkipImages)     ret |= (uint)html_render_mode.HTML_SKIP_IMAGES;
 			if (SkipLinks)      ret |= (uint)html_render_mode.HTML_SKIP_LINKS;
-			if (SkipExpandTabs) ret |= (uint)html_render_mode.HTML_SKIP_EXPAND_TABs;
+			if (SkipExpandTabs) ret |= (uint)html_render_mode.HTML_SKIP_EXPAND_TABS;
 			if (SafeLink)       ret |= (uint)html_render_mode.HTML_SAFELINK;
 			if (TOC)            ret |= (uint)html_render_mode.HTML_TOC;
 			if (HardWrap)       ret |= (uint)html_render_mode.HTML_HARD_WRAP;
 			if (UseXHTML)       ret |= (uint)html_render_mode.HTML_USE_XHTML;
 			if (Escape)         ret |= (uint)html_render_mode.HTML_ESCAPE;
-			
+
 			return ret;
 		}
 	}
